35251b08aff9349f8d7d33b1ca09a938cbe553f4
cor <cor@pruijs.nl>
2020-04-24 16:54:05 +0200

    Add missing Console.Write call

--- domjudge-version-extractor/Program.cs
+++ domjudge-version-extractor/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace domjudge_version_extractor
+﻿using System;
+
+namespace domjudge_version_extractor
 {
-  internal class Program
-  {
-    public static void Main(string[] args)
+    internal class Program
     {
+        public static void Main(string[] args)
+        {
+            Console.Write(typeof(string).Assembly.ImageRuntimeVersion);
+        }
     }
-  }
 }
